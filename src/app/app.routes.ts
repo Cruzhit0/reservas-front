700e5e8b97a53a7def3e831a74d861e0af9cca48
carlos_cruz <carlos.cruz@ucb.edu.bo>
2025-06-11 12:04:47 -0400

    feat: aplicacion de las ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import type { Routes } from "@angular/router"
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/espacios",
+    pathMatch: "full",
+  },
+  {
+    path: "auth",
+    loadChildren: () => import("./features/auth/auth.routes").then((m) => m.authRoutes),
+  },
+  {
+    path: "espacios",
+    loadChildren: () => import("./features/espacios/espacios.routes").then((m) => m.espaciosRoutes),
+  },
+  {
+    path: "**",
+    redirectTo: "/espacios",
+  },
+]
